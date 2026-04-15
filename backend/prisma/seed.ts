bf38cac0f886411c344a71568df4eb67bef065ff
NathanWSLIFSC <nathaniel.christian2077@gmail.com>
2026-04-15 09:56:18 -0300

    commit

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,6 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import 'dotenv/config'
+import { PrismaClient, Prisma } from './generated/client';
+import { PrismaPg } from '@prisma/adapter-pg'
 import bcrypt from "bcrypt";
-const prisma = new PrismaClient();
+
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+export const prisma = new PrismaClient({ adapter: pool })
 
 async function main() {
   const password = await bcrypt.hash("123456", 10);
--- backend/src/common/prisma.service.ts
+++ backend/src/common/prisma.service.ts
@@ -1,9 +1,15 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client";
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    async onModuleInit() {
+  constructor() {
+    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+    super({ adapter: pool })
+  }
+
+  async onModuleInit() {
         await this.$connect();
     }
     async onModuleDestroy() {
